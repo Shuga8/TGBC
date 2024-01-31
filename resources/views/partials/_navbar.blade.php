37c020904ba0636573b6b63e39b4e01f1e277f1e
Shuga8 <lotocharles8@gmail.com>
2024-01-31 05:02:15 +0100

    navabr links

---
+++ resources/views/partials/_navbar.blade.php
@@ -0,0 +1,43 @@
+<header class="header" id="header">
+	<nav class="navbar" id="navbar">
+		<div class="logo">
+			<a href="{{ route('home') }}"><img src="{{ asset('logo_300X300.png') }}" alt="Logo"></a>
+		</div>
+
+		<div class="links">
+			<div class="link">
+				<a href="">Home</a>
+			</div>
+
+			<div class="link">
+				<a href="">About</a>
+
+				<div class="dropdown about-dropdown">
+					<a href="">Meet Our G.O</a>
+					<a href="">Our Mummy G.O</a>
+					<a href="">Meet Our Pastors</a>
+				</div>
+			</div>
+
+			<div class="link">
+				<a href="">Sermons</a>
+			</div>
+
+			<div class="link">
+				<a href="">Events</a>
+			</div>
+
+			<div class="link">
+				<a href="">Blog</a>
+			</div>
+
+			<div class="link">
+				<a href="">Contact Us</a>
+			</div>
+
+			<div class="link">
+				<a class="donate-btn" href="">Donate</a>
+			</div>
+		</div>
+	</nav>
+</header>
