537c8ca66ab2a3a9048e86cd23c4d4a10d884eec
Shuga8 <lotocharles8@gmail.com>
2024-02-10 01:08:41 +0100

    persisting

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -14,24 +14,52 @@
 			<div class="slide first">
 				<img src="{{ asset('images/1.jpg') }}" alt="">
 				<div class="content first">
-					<h1 class="slide-up">{{ config('app.name') }}</h1>
+					<h1 class="">{{ config('app.name') }}</h1>
 
-					<div class="actions">
+					<div class="actions slide-up">
 						<a class="more" href="">More</a>
+						<a class="donate" href="">Donate</a>
 					</div>
 				</div>
 			</div>
 
 			<div class="slide">
 				<img src="{{ asset('images/2.jpg') }}" alt="">
+
+				<div class="content">
+					<h1 class="">{{ config('app.name') }}</h1>
+
+					<div class="actions">
+						<a class="more" href="">More</a>
+						<a class="donate" href="">Donate</a>
+					</div>
+				</div>
 			</div>
 
 			<div class="slide">
 				<img src="{{ asset('images/3.jpg') }}" alt="">
+
+				<div class="content">
+					<h1 class="">{{ config('app.name') }}</h1>
+
+					<div class="actions">
+						<a class="more" href="">More</a>
+						<a class="donate" href="">Donate</a>
+					</div>
+				</div>
 			</div>
 
 			<div class="slide">
 				<img src="{{ asset('images/4.jpg') }}" alt="">
+
+				<div class="content">
+					<h1 class="">{{ config('app.name') }}</h1>
+
+					<div class="actions">
+						<a class="more" href="">More</a>
+						<a class="donate" href="">Donate</a>
+					</div>
+				</div>
 			</div>
 
 
@@ -53,6 +81,20 @@
 			</div>
 		</div>
 	</div>
+
+	@push('scripts')
+		<script type="text/javascript">
+			let counter = 1;
+
+			setInterval(() => {
+				document.getElementById("radio" + counter).checked = true;
+				counter++;
+				if (counter > 4) {
+					counter = 1;
+				}
+			}, 3000);
+		</script>
+	@endpush
 
 
 </x-layout>
