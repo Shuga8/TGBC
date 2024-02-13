d46bb668cabd0be081d9e3bcbd1f9348500ae26e
Shuga8 <lotocharles8@gmail.com>
2024-02-13 22:59:15 +0100

    mummy g.o
    finishing

--- resources/views/pages/mummy_go.blade.php
+++ resources/views/pages/mummy_go.blade.php
@@ -1,3 +1,39 @@
-<x-layout title="$title">
+<x-layout :title="$title">
+
+	<section class="mummy-overseer-page">
+		<div class="hero">
+
+			<div class="overlay p-1">
+				<h2 class="slide-up text-center text-3xl text-white">Meet Our Mummy G.O</h2>
+			</div>
+		</div>
+
+		<div class="overseer-content px-3 py-8">
+			<div class="content">
+				<h3 class="text-2xl font-extrabold text-gray-950">Rev Mrs Akingbelure</h3>
+
+				<blockquote class="p-2 text-gray-600">
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi recusandae possimus obcaecati, a expedita culpa.
+					</p>
+					<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor facere hic porro itaque praesentium, eum numquam
+						dolore veritatis nam?</p>
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum ducimus corporis cupiditate, voluptatem
+						temporibus soluta aspernatur odio.</p>
+
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo autem, beatae iure placeat dolor aspernatur sint
+						consequuntur nemo reprehenderit odio! Placeat, porro. Nostrum debitis ipsam exercitationem reiciendis in amet
+						pariatur numquam reprehenderit vero? Quidem necessitatibus ratione error hic quas doloribus?
+					</p>
+
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam suscipit tempora mollitia dolorem nesciunt
+						tempore vel commodi, ut maiores laudantium.</p>
+				</blockquote>
+			</div>
+			<figure>
+				<img src="{{ asset('images/SXP_2019 (2).jpg') }}" alt="" loading="lazy">
+			</figure>
+		</div>
+	</section>
 
 </x-layout>
