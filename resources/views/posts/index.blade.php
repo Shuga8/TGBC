72cb551a6ddd4cae615667b77aa13a08d554e7da
Shuga8 <lotocharles8@gmail.com>
2024-11-08 13:49:18 +0100

    center not found

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,47 +1,49 @@
 <x-layout :title="$title">
-	<section class="blog-page">
-		<div class="hero">
+    <section class="blog-page">
+        <div class="hero">
 
-			<div class="overlay p-1">
-				<h2 class="slide-up text-center text-3xl text-red-700">{{ $title }}</h2>
-			</div>
-		</div>
+            <div class="overlay p-1">
+                <h2 class="slide-up text-center text-3xl text-red-700">{{ $title }}</h2>
+            </div>
+        </div>
 
-		<div class="content px-2 py-5">
-			<div class="posts">
-				@unless ($posts->count() == 0)
-					@foreach ($posts as $post)
-						<div class="post block">
-							<figure>
-								<a href="{{ route('blog.single', $post->id) }}">
-									<img src="{{ asset('storage/' . $post->image_url) }}" alt="">
-								</a>
-							</figure>
+        <div class="content px-2 py-5">
+            @unless ($posts->count() == 0)
+                <div class="posts">
 
-							<div class="content px-2 pb-2">
-								<date class="date flex flex-row justify-end text-teal-600"><x-heroicon-o-calendar-days
-										class="h-6 w-4 text-teal-900" />&nbsp;{{ $post->published_at }}</date>
+                    @foreach ($posts as $post)
+                        <div class="post block">
+                            <figure>
+                                <a href="{{ route('blog.single', $post->id) }}">
+                                    <img src="{{ asset('storage/' . $post->image_url) }}" alt="">
+                                </a>
+                            </figure>
 
-								<p class="text-sm font-semibold uppercase"><a
-										href="{{ route('blog.single', $post->id) }}">{{ $post->title }}</a></p>
+                            <div class="content px-2 pb-2">
+                                <date class="date flex flex-row justify-end text-teal-600"><x-heroicon-o-calendar-days
+                                        class="h-6 w-4 text-teal-900" />&nbsp;{{ $post->published_at }}</date>
 
-								<div class="post-content mb-2">
-									{!! $post->content !!}
-								</div>
+                                <p class="text-sm font-semibold uppercase"><a
+                                        href="{{ route('blog.single', $post->id) }}">{{ $post->title }}</a></p>
 
-								<a class="more-btn" href="{{ route('blog.single', $post->id) }}">more</a>
-							</div>
-						</div>
-					@endforeach
-				@else
-					<div class="text-center text-2xl font-bold text-red-800">No posts found!</div>
-				@endunless
-			</div>
+                                <div class="post-content mb-2">
+                                    {!! $post->content !!}
+                                </div>
 
-			<div class="pagination-links p-2">
-				{{ $posts->links() }}
-			</div>
-		</div>
+                                <a class="more-btn" href="{{ route('blog.single', $post->id) }}">more</a>
+                            </div>
+                        </div>
+                    @endforeach
+
+                </div>
+            @else
+                <div class="text-center text-2xl font-bold text-red-800">No posts found!</div>
+            @endunless
+
+            <div class="pagination-links p-2">
+                {{ $posts->links() }}
+            </div>
+        </div>
 
 
 </x-layout>
--- resources/views/sermons/index.blade.php
+++ resources/views/sermons/index.blade.php
@@ -1,37 +1,37 @@
 <x-layout :title="$title">
 
-	<section class="sermons-page">
-		<div class="hero">
+    <section class="sermons-page">
+        <div class="hero">
 
-			<div class="overlay p-1">
-				<h2 class="slide-up text-center text-3xl text-red-700">{{ $title }}</h2>
-			</div>
-		</div>
+            <div class="overlay p-1">
+                <h2 class="slide-up text-center text-3xl text-red-700">{{ $title }}</h2>
+            </div>
+        </div>
 
-		<div class="content px-4 py-14">
-			@unless ($sermons->count() == 0)
-				<div class="sermons">
-					@foreach ($sermons as $sermon)
-						<div class="sermon">
-							<h4 class="mb-3 font-semibold uppercase text-teal-700">{{ $sermon->title }}</h4>
-							<audio controls width="100%">
-								<source src="{{ asset('storage/' . $sermon->file_url) }}" type="audio/mp3">
-							</audio>
-							<div class="inner-content mt-4">
-								{!! $sermon->description !!}
-							</div>
-						</div>
-					@endforeach
-				</div>
-			@else
-				<p class="text-center text-2xl font-semibold text-red-800">No Sermons Found!</p>
-			@endunless
-		</div>
+        <div class="content px-4 py-14">
+            @unless ($sermons->count() == 0)
+                <div class="sermons">
+                    @foreach ($sermons as $sermon)
+                        <div class="sermon">
+                            <h4 class="mb-3 font-semibold uppercase text-teal-700">{{ $sermon->title }}</h4>
+                            <audio controls width="100%">
+                                <source src="{{ asset('storage/' . $sermon->file_url) }}" type="audio/mp3">
+                            </audio>
+                            <div class="inner-content mt-4">
+                                {!! $sermon->description !!}
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            @else
+                <p class="text-center text-2xl font-semibold text-red-800">No Sermons Found!</p>
+            @endunless
+        </div>
 
-		<div class="pagination-links p-1">
-			{{ $sermons->links() }}
-		</div>
+        <div class="pagination-links p-1">
+            {{ $sermons->links() }}
+        </div>
 
-	</section>
+    </section>
 
 </x-layout>
